3ddef15d2cc12a431356cc15de45fb607a745d36
ArwaSalah1 <arwasalah256@gmail.com>
2023-09-12 12:04:14 -0700

    newwwwww

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,19 @@
 #include "main.h"
 
 /**
- * print_sign - prints the sign of a numver
+ * print_sign - print + if n is greater than 0
+ *             0 if n is zero and - if n is leaa than zero
  *
- * @n: the int to check
+ * @n: takes integer type input for function
  *
- * Return: 1 and prints + if n is greater than zero
- *
- * 0 and prints 0 if n is zero
- *
- * -1 and prints - if n is less than zero
- */
+ * Return: 1 if +, 0 if 0 and -1 if -
+*/
 
 int print_sign(int n)
 {
         if (n > 0)
         {
-                _putchar('+');
+                _putchar(43);
                 return (1);
         }
         else if (n == 0)
@@ -24,9 +21,9 @@ int print_sign(int n)
                 _putchar(48);
                 return (0);
         }
-        else if (n < 0)
+        else
         {
-                _putchar('-');
+                _putchar(45);
+                return (-1);
         }
-        return (-1);
 }
